7e67f26933d28d186f67c9aa4281a4d1a8c6cc8e
ethanimooney <ethanimooney@gmail.com>
2023-02-06 14:18:26 -0600

    Remove old code, setup for new development

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,14 +7,10 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        darkBlue: "#627985",
-        lightBlue: "#b2bfc6",
-        lightBlueDarker: "#919DA4",
-        lightGray: "#ececec",
-        lightRed: "#cd9a9a",
-        darkerRed: "#9e7676",
-        lightGreen: "#a4cd9a",
-        darkerGreen: "#7e9e75",
+        darkBlue: "#2E6799",
+        mediumBlue: "#3F86C5",
+        lightBlue: "#B8D3EA",
+        darkGray: "#343434",
       },
     },
   },
---
+++ src/views/StudentHomeDashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container fluid> Dashboard content </v-container>
+</template>
+
+<script>
+  export default {
+    name: "StudentHomeDashboard",
+    components: {},
+    data() {
+      return {};
+    },
+    methods: {},
+  };
+</script>
