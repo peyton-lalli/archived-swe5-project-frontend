f6c1545ec981680744eaa105604ef0c989629169
Peyton Lalli <peyton.lalli@eagles.oc.edu>
2023-02-11 11:53:09 -0600

    Start of the nav bar for the website

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
   <v-app>
-    <v-app-bar app color="darkBlue" dark>
-      <v-img
-        alt="OC Logo"
-        class="shrink mr-4"
-        contain
-        src="./assets/OC_LOGO_WHITE.svg"
-        transition="scale-transition"
-        width="40"
-      />
-
-      <v-toolbar-title 
-        color = "mediumBlue"
-        class = "font-weight-bold">MUSIC DEPARTMENT</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-      </v-toolbar-items>
-    </v-app-bar>
-    <v-main>
-      <v-container> </v-container>
-    </v-main>
+    <MenuBar></MenuBar>
   </v-app>
 </template>
 
 <script>
+import MenuBar from "./components/MenuBar";
 export default {
   name: "App",
+  components: {
+    MenuBar,
+  },
 };
 </script>
