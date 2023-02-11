cea2256d7db1f7676387774ba2dc81d462bb758a
Peyton Lalli <peyton.lalli@eagles.oc.edu>
2023-02-11 11:27:11 -0600

    This is the start of the nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <v-app>
-    <v-container fluid>
-      //Add MenuBar component here
+    <v-app-bar app color="darkBlue" dark>
+      <v-img
+        alt="OC Logo"
+        class="shrink mr-4"
+        contain
+        src="./assets/OC_LOGO_WHITE.svg"
+        transition="scale-transition"
+        width="40"
+      />
 
-      <v-row>
-        <v-col cols="3"> Static user Nav </v-col>
-        <v-col cols="9">
-          <v-main>
-            <router-view :key="$route.fullPath"></router-view>
-          </v-main>
-        </v-col>
-      </v-row>
-    </v-container>
+      <v-toolbar-title 
+        color = "mediumBlue"
+        class = "font-weight-bold">MUSIC DEPARTMENT</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-main>
+      <v-container> </v-container>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-  export default {
-    name: "App",
-    components: {},
-    data: () => ({}),
-  };
+export default {
+  name: "App",
+};
 </script>
